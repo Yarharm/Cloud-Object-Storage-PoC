33aa056445f568b1a505f04a5fbc4bfda1ce4973
John-Estrada <jestrada740@gmail.com>
2020-10-18 09:03:12 -0400

    implemented basic REST api

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+class FileUpload(models.Model):
+    title = models.CharField(max_length=64, default='unnamed_file')
+    file = models.FileField(null=True, upload_to='files/')
+
+    def __str__(self):
+        return self.title
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import FileUpload
+from rest_framework import viewsets
+from .serializers import FileUploadSerializer
 
 # Create your views here.
+def home(request):
+    context = {'files':'placeholder'}
+    all_files = FileUpload.objects.all()
+    context['files'] = FileUpload.objects.all()
+
+    return render(request, 'base.html', context)
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = FileUpload.objects.all().order_by('title')
+    serializer_class = FileUploadSerializer
