767ff73e0908a61ce88cd1d3b14f735eadb2e942
Yaroslav Harmatyuk <42894823+Yarharm@users.noreply.github.com>
2020-11-13 17:45:32 -0500

    intergrate aws bucket (#5)
    * intergrate aws bucket
    
    * added bucket name constant to settings.py
    
    Co-authored-by: John-Estrada <jestrada740@gmail.com>

--- core/views.py
+++ core/views.py
@@ -1,4 +1,17 @@
+from cloudstorage.settings import STATIC_URL, AWS_BUCKET_NAME
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import boto3
+
+@csrf_exempt
+def aws_post(request):
+    file = request.FILES['file']
+    file_name = file.name
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(AWS_BUCKET_NAME)
+    bucket.put_object(Key=file_name, Body=file)
+    url = f'https://{AWS_BUCKET_NAME}.s3.amazonaws.com/{file_name}'
+    return JsonResponse({'fileUrl': url})
 
 
 def health(request):
